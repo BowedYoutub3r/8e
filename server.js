f8da940f1073ee9d366bafcf072938f9b2886160
CEDRICDESAYT <48097454+CEDRICDESAYT@users.noreply.github.com>
2021-08-14 18:41:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require("express")();
-var http = require("http").Server(app);
+var http = require("https").Server(app);
 var fs = require("fs");
-var io = require("socket.io")(http);
+var io = require("socket.io")(https);
 
 // handling files
 app.get('/', onRequest);
